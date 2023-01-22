cad0f0dc4a8791909fd29a26ad53a71211686a98
trojan1pigeon <123129441+trojan1pigeon@users.noreply.github.com>
2023-01-23 00:46:34 +0300

    Add files via upload

---
+++ portfolio2/js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+const swiper = new Swiper('.swiper', {
+	pagination: {
+		el: '.project-pagination',
+		bulletClass: 'project-bullet',
+		bulletActiveClass: 'project-bullet-active',
+		clickable: true
+	},
+  });
